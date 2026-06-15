5dcc3913cbf0863042e38f0a57519fa32c12dfb3
Pablo Fernandez Mila <pablofernandezmila@ba.com>
2026-06-14 23:37:46 -0300

    Revert "removed gallery autoplay"
    This reverts commit 8425575443a23816b6dfbaeedef25dad78a28cef.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -20,12 +20,13 @@ export default function Gallery() {
     <section className={styles.wrapper}>
       <div className={styles.container}>
         <Swiper
-          modules={[Pagination, EffectFade]}
+          modules={[Pagination, EffectFade, Autoplay]}
           spaceBetween={0}
           slidesPerView={1}
           effect="fade"
           fadeEffect={{ crossFade: true }}
           loop={true}
+          autoplay={{ delay: 4000, disableOnInteraction: false }}
           pagination={{
             clickable: true,
             bulletClass: styles.bullet,
